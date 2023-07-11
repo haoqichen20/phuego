3874d0fef5f037797eca8afdbfea02027e302e05
hchen_codon <hchen@ebi.ac.uk>
2023-07-11 12:29:23 +0100

    Reorganize click.options into several groups. Didn't figure out how to print help message according to groups.

--- phuego/main.py
+++ phuego/main.py
@@ -13,50 +13,62 @@ __version__ = "0.1.0.dev5"
 This is the CLI tool for phuego.
 '''
 @click.command()
+# Folders.
 @click.option("--support_data_folder", "-sf", type=str, required=False, 
               help="Folder that stored support data. Directories separated by /")
 @click.option("--res_folder", "-rf", type=str, required=False, 
               help="Folder that stored output result. Directories separated by /")
+@click.option("--convert2folder", "-c2f", default=False, type=bool, 
+              required=False, help="Should phuego organize result files into a folder structure?")
+@click.option("--test_path", "-tpath", default="", type=str, required=False, 
+              help="The path of user input test file. If defined, -mock and -test should not be used.")
+
+# Support dataset download.
+@click.option("--need_fisher", default=False, type=bool, required=False, 
+              help="Should phuego download the fisher geneset from zenodo?")
+@click.option("--need_gic_sim", default=False, type=bool, required=False, 
+              help="Should phuego download the semantic similarity from zenodo?")
+@click.option("--need_networks", default=False, type=bool, required=False, 
+              help="Should phuego download the networks from zenodo?")
+@click.option("--remove_zip_file", default=False, type=bool, required=False, 
+              help="Should phuego remove the zip files after downloading? In a shared folder system it's recommended to do so.")
+
+# Mock/test.
+@click.option("--run_mock", "-mock", default=False, type=bool, required=False, 
+              help="Should phuego perform a mock test run using test data? If defined, -test and -tpath would be ignored.")
+@click.option("--run_test", "-test", default=False, type=bool, required=False, 
+              help="Should phuego perform a full test run using test data? If defined, -tpath would be ignored.")
+
+
+# network propagation parameters.
 @click.option("--use_existing_rwr", "-ru", type=bool, required=False, 
               help="Should phuego reuse existing network propagation results?")
-@click.option("--convert2folder", default=False, type=bool, 
-              required=False, help="Should phuego organize result files in a folder structure?")
+@click.option("--damping", "-d", default=0.85, type=float, required=False, 
+              help="Damping factor for random walk with restart algorithm. Float number within range [0.5, 0.95]")
+@click.option("--ini_pos", "-ip", default=['False'], type=str, multiple=True, 
+              required=False, 
+              help="List of nodes to be removed from network for the propagation of upregulated seeds, such as targets of a drug in a drugging experiment. Normally the same as ini_neg. If no node to be removed, leave as default.")
+@click.option("--ini_neg", "-in", default=['False'], type=str, multiple=True, 
+              required=False, 
+              help="List of nodes to be removed from network for the propagation of downregulated seeds, such as targets of a drug in a drugging experiment. Normally the same as ini_pos. If no node to be removed, leave as default.")
 
-#One of the following three options need to be True or non-empty.
-@click.option("--run_mock", "-mock", default=False, type=bool, 
-       required=False, help="Should phuego perform a mock test run using test data? If defined, -test and -tpath would be ignored.")
-@click.option("--run_test", "-test", default=False, type=bool, 
-       required=False, help="Should phuego perform a full test run using test data? If defined, -tpath would be ignored.")
-@click.option("--test_path", "-tpath", default="", type=str, 
-       required=False, help="The path of user input test file. If defined, -mock and -test should not be used.")
-
-@click.option("--need_fisher", default=False, type=bool, 
-       required=False, help="Should phuego download the fisher geneset from zenodo?")
-@click.option("--need_gic_sim", default=False, type=bool, 
-       required=False, help="Should phuego download the semantic similarity from zenodo?")
-@click.option("--need_networks", default=False, type=bool, 
-       required=False, help="Should phuego download the networks from zenodo?")
-@click.option("--remove_zip_file", default=False, type=bool, 
-       required=False, help="Should phuego remove the zip files after downloading? In a shared folder system it's recommended to do so.")
-
-@click.option("--damping", "-d", default=0.85, type=float, 
-       required=False, help="Damping factor for random walk with restart algorithm. Float number within range [0.5, 0.95]")
-
-@click.option("--fisher_geneset", "-fg", default=["C","F","D","P","R","K","RT","B"], type=str, multiple=True,
-       required=False, help="Abbreviation of genesets to be tested when annotating modules of propagation, can be one of 'C,F,D,P,R,K,RT,B'. Multiple genesets can be provided at the same time by reusing the arguments. Refer to documents to learn what each abbreviation stands for.")
-@click.option("--fisher_threshold", "-ft", default=0.05, type=float, 
-       required=False, help="Threshold of significance of geneset enrichment analysis") 
-@click.option("--fisher_background", "-fb", default="intact", type=str, 
-       required=False, help="Geneset background of geneset enrichment analysis") 
-
+# ego propagation and fisher test.
 @click.option("--kde_cutoff", "-k", default=[0.85, 0.9], type=float, multiple=True,
-       required=False, help="KDE cutoff value for removing less similar nodes in ego network. A float number within range [0.5, 0.95]. Multiple numbers can be provided at the same time by reusing the arguments.")
-@click.option("--ini_pos", "-ip", default=['False'], type=str, multiple=True,
-       required=False, help="List of nodes to be removed from network for the propagation of upregulated seeds, such as targets of a drug in a drugging experiment. Normally the same as ini_neg. If no node to be removed, leave as default.")
-@click.option("--ini_neg", "-in", default=['False'], type=str, multiple=True,
-       required=False, help="List of nodes to be removed from network for the propagation of downregulated seeds, such as targets of a drug in a drugging experiment. Normally the same as ini_pos. If no node to be removed, leave as default.")
+              required=False, 
+              help="KDE cutoff value for removing less similar nodes in ego network. A float number within range [0.5, 0.95]. Multiple numbers can be provided at the same time by reusing the arguments.")
+@click.option("--fisher_geneset", "-fg", default=["C","F","D","P","R","K","RT","B"], type=str, multiple=True,
+              required=False, 
+              help="Abbreviation of genesets to be tested when annotating modules of propagation, can be one of 'C,F,D,P,R,K,RT,B'. Multiple genesets can be provided at the same time by reusing the arguments. Refer to documents to learn what each abbreviation stands for.")
+@click.option("--fisher_threshold", "-ft", default=0.05, type=float, 
+              required=False, 
+              help="Threshold of significance of geneset enrichment analysis") 
+@click.option("--fisher_background", "-fb", default="intact", type=str, 
+              required=False, 
+              help="Geneset background of geneset enrichment analysis") 
 
-@click.option('--version', is_flag=True, help='Print version to stdout')
+# Versioning.
+@click.option('--version', '-v', is_flag=True, 
+              help='Print version to stdout')
 
 def main(support_data_folder, res_folder, test_path, convert2folder, use_existing_rwr, run_test, run_mock, 
          need_fisher, need_gic_sim, need_networks, remove_zip_file,
