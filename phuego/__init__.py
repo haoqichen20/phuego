97c30e550b3b705a5e769b2262387b8d76126b6c
hchen_codon <hchen@ebi.ac.uk>
2023-07-28 12:45:42 +0100

    update __init__.py

--- phuego/__init__.py
+++ phuego/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from .dataprep import dataprep
-from .load_example import load_test_example
+from .utils_CLI import dataprep
+from .utils_CLI import load_test_example
 from .phuego import phuego
 from .phuego_mock import phuego_mock
 
 from .load_seeds import load_seeds
-from .rwr_values import rwr_values
-from .pvalue_splitting import pvalue_split
-from .ego2module import test_function
+from .network_rwr import rwr_values
+from .network_rwr import pvalue_split
 from .ego import ego_filtering
+from .ego2module import merge_egos
 from .generate_net import generate_nets
