0d1d4b3b642445006165eca12cd80d11cc11d38a
hchen_codon <hchen@ebi.ac.uk>
2023-10-26 15:22:07 +0100

    Update convert_result() function to fix the kde mapping bug.

--- phuego/utils.py
+++ phuego/utils.py
@@ -212,7 +212,7 @@ def convert_result(res_folder, kde_cutoff, net_format):
             direction_KDE_rf = direction_rf+"KDE_"+str(kde)+"/"
             os.mkdir(direction_KDE_rf)
             # Move KDE_egos.txt.
-            srcf = [file_name for file_name in direction_files if "_sig_cluster_" in file_name and str(kde) in file_name]
+            srcf = [file_name for file_name in direction_files if "_sig_cluster_" in file_name and str(kde)+".txt" in file_name]
             # there should only be one _sig_cluster_ file per kde x direction.
             if(len(srcf)>1):
                 sys.exit(f"More than one _sig_cluster_ file is present for '{direction}'. KDE is {kde}")
@@ -224,7 +224,7 @@ def convert_result(res_folder, kde_cutoff, net_format):
             direction_KDE_fisher_rf = direction_KDE_rf + "fisher/"
             os.mkdir(direction_KDE_fisher_rf)
             # Move all fisher files for signature.
-            srcf = [file_name for file_name in direction_files if "_sig_fisher_" in file_name and str(kde) in file_name]
+            srcf = [file_name for file_name in direction_files if "_sig_fisher_" in file_name and str(kde)+"_" in file_name]
             for file in srcf:
                 new_file = file.split("_")[-1]
                 shutil.move(src=res_folder+file, dst=direction_KDE_fisher_rf+new_file)
@@ -233,7 +233,7 @@ def convert_result(res_folder, kde_cutoff, net_format):
             direction_KDE_networks_rf = direction_KDE_rf + "networks/"
             os.mkdir(direction_KDE_networks_rf)
             # Move supernodes_net.
-            srcf = [file_name for file_name in direction_files if "_supernodes_net_" in file_name and str(kde) in file_name]
+            srcf = [file_name for file_name in direction_files if "_supernodes_net_" in file_name and str(kde)+".txt" in file_name]
             # There should only be one supernodes_net per kde x direction.
             if(len(srcf)>1):
                 sys.exit(f"More than one supernodes_net is present for '{direction}'. KDE is {kde}")
@@ -241,12 +241,14 @@ def convert_result(res_folder, kde_cutoff, net_format):
                 new_file = "supernodes_net.txt"
                 shutil.move(src=res_folder+file, dst=direction_KDE_networks_rf+new_file)
             # Move sig_net and module_net
-            srcf = [file_name for file_name in direction_files if net_format in file_name and str(kde) in file_name]
+            # srcf = [file_name for file_name in direction_files if net_format in file_name and str(kde) in file_name]
+            srcf = [file_name for file_name in direction_files if str(kde)+"."+net_format in file_name]
             for file in srcf:
                 new_file = file.replace("_"+direction+"_"+str(kde), "")
                 shutil.move(src=res_folder+file, dst=direction_KDE_networks_rf+new_file)
             # Move module_net csv file.
-            srcf = [file_name for file_name in direction_files if "module_net_" in file_name and ".csv" in file_name and str(kde) in file_name]
+            # srcf = [file_name for file_name in direction_files if "module_net_" in file_name and ".csv" in file_name and str(kde) in file_name]
+            srcf = [file_name for file_name in direction_files if "module_net_" in file_name and str(kde)+".csv" in file_name]
             for file in srcf:
                 # new_file = "module_net_annotated_edgelist.csv"
                 new_file = file.replace("_"+direction, "")
@@ -259,7 +261,7 @@ def convert_result(res_folder, kde_cutoff, net_format):
             # Collect the module names for the regulation direction.
             direction_module_names = set()
             for file_name in direction_files:
-                if("module_" in file_name and "cluster_"+str(kde) in file_name):
+                if("module_" in file_name and "cluster_"+str(kde)+".txt" in file_name):
                     module = file_name.split("_")[2]
                     module = "module_" + module
                     direction_module_names.add(module)
@@ -270,7 +272,7 @@ def convert_result(res_folder, kde_cutoff, net_format):
                 direction_KDE_modules_module_rf = direction_KDE_modules_rf+module+"/"
                 os.mkdir(direction_KDE_modules_module_rf)
                 # Move module_egos file.
-                srcf = [file_name for file_name in direction_files if module in file_name and "_cluster_" in file_name and str(kde) in file_name]
+                srcf = [file_name for file_name in direction_files if module in file_name and "_cluster_" in file_name and str(kde)+".txt" in file_name]
                 for file in srcf:
                     new_file = "module_egos.txt"
                     shutil.move(res_folder+file, direction_KDE_modules_module_rf+new_file)
@@ -279,7 +281,7 @@ def convert_result(res_folder, kde_cutoff, net_format):
                 direction_KDE_modules_module_fisher_rf = direction_KDE_modules_rf+module+"/fisher/"
                 os.mkdir(direction_KDE_modules_module_fisher_rf)
                 # Move module fisher files.
-                srcf = [file_name for file_name in direction_files if module in file_name and "_fisher_" in file_name and str(kde) in file_name]
+                srcf = [file_name for file_name in direction_files if module in file_name and "_fisher_" in file_name and str(kde)+"_" in file_name]
                 for file in srcf:
                     new_file = file.split("_")[-1]
                     shutil.move(res_folder+file, direction_KDE_modules_module_fisher_rf+new_file)
