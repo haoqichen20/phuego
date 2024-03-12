9514c97fb6a1ad380b57344c33502e5c4ec17a5b
hchen_codon <hchen@ebi.ac.uk>
2024-03-12 22:51:09 +0000

    add PythonLiteralOption class for parsing user input list literally

--- phuego/utils_CLI.py
+++ phuego/utils_CLI.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import os
+import ast
+import click
 import pandas as pd
+
     
 def load_test_example():
     test_path = os.path.join(os.path.dirname(__file__), 'data/EGF_vs_Untreated_@min_15_63_240.txt')
     df = pd.read_csv(test_path, sep='\t', header=None)
     return test_path, df
+
+
+# Parsing user input literally for list input.
+class PythonLiteralOption(click.Option):
+
+    def type_cast_value(self, ctx, value):
+        try:
+            return ast.literal_eval(value)
+        except:
+            raise click.BadParameter(value)
