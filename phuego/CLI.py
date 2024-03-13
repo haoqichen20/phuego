07ae7c389e2534490cde04ccbc4dd7372db9980a
hchen_codon <hchen@ebi.ac.uk>
2024-03-13 00:16:47 +0000

    Remove code of converting tuple into list, and add assertion. Removed unused required=false option configuration.

--- phuego/CLI.py
+++ phuego/CLI.py
@@ -19,27 +19,27 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 # Folders.
-@click.option("--support_data_folder", "-sf", type=str, required=False, 
+@click.option("--support_data_folder", "-sf", type=str, 
               help="Folder that stored support data. Directories separated by /")
-@click.option("--res_folder", "-rf", type=str, required=False, 
+@click.option("--res_folder", "-rf", type=str, 
               help="Folder that stored output result. Directories separated by /")
-@click.option("--test_path", "-tpath", default="", type=str, required=False, 
+@click.option("--test_path", "-tpath", default="", type=str, 
               help="The path of user input test file. If defined, -mock and -test should not be used.")
-@click.option("--ini_path", "-ipath", default="", type=str, required=False, 
+@click.option("--ini_path", "-ipath", default="", type=str, 
               help="The path of a csv file, that specify two sets of nodes to be removed from reference network during propagation.")
-@click.option("--convert2folder/--dont_convert2folder", default=True, required=False, 
+@click.option("--convert2folder/--dont_convert2folder", default=True, 
               help="Should phuego organize result files into a folder structure?")
 # Mock/test.
-@click.option("--run_mock", is_flag=True, required=False, 
+@click.option("--run_mock", is_flag=True, 
               help="Should phuego perform a mock test run using test data? If defined, -test and -tpath would be ignored.")
-@click.option("--run_test", is_flag=True, required=False, 
+@click.option("--run_test", is_flag=True, 
               help="Should phuego perform a full test run using test data? If defined, -tpath would be ignored.")
 # network propagation parameters.
-@click.option("--use_existing_rwr", "-ru", is_flag=True, required=False, 
+@click.option("--use_existing_rwr", "-ru", is_flag=True, 
               help="Should phuego reuse existing network propagation results?")
-@click.option("--damping", "-d", default=0.85, type=float, required=False, 
+@click.option("--damping", "-d", default=0.85, type=float, 
               help="Damping factor for random walk with restart algorithm. Float number within range [0.5, 0.95]")
-@click.option("--rwr_threshold", "-rt", default=0.05, type=float, required=False, 
+@click.option("--rwr_threshold", "-rt", default=0.05, type=float, 
               help="Threshold of significance for propagated nodes. Float number within range [0.01, 0.1]")
 # ego propagation and fisher test.
 @click.option("--kde_cutoff", "-k", cls=PythonLiteralOption, default="[0.85]", 
@@ -50,23 +50,15 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
                help=("Abbreviation of genesets to be tested when annotating modules of propagation,"
                      " can be one of 'C,F,D,P,R,K,RT,B'. Multiple genesets can be provided at the same time by reusing the argument flag."
                      " Refer to documents to learn what each abbreviation stands for."))
-# @click.option("--kde_cutoff", "-k", default=[0.85], type=float, multiple=True,
-#               required=False, 
-#               help="KDE cutoff value for removing less similar nodes in ego network. A float number within range [0.5, 0.95]. Multiple numbers can be provided at the same time by reusing the argument flag.")
-# @click.option("--fisher_geneset", "-fg", default=["K"], type=str, multiple=True,
-#               required=False, 
-#               help="Abbreviation of genesets to be tested when annotating modules of propagation, can be one of 'C,F,D,P,R,K,RT,B'. Multiple genesets can be provided at the same time by reusing the argument flag. Refer to documents to learn what each abbreviation stands for.")
-@click.option("--fisher_threshold", "-ft", default=0.05, type=float, 
-              required=False, 
+@click.option("--fisher_threshold", "-ft", default=0.05, type=float,
               help="Threshold of significance of geneset enrichment analysis") 
-@click.option("--fisher_background", "-fb", default="intact", type=str, 
-              required=False, 
+@click.option("--fisher_background", "-fb", default="intact", type=str,
               help="Geneset background of geneset enrichment analysis") 
 
 # network output
-@click.option("--include_isolated_egos_in_kde_net", "-ie", is_flag=True, required=False, 
+@click.option("--include_isolated_egos_in_kde_net", "-ie", is_flag=True, 
               help="Should we include isolated nodes in the output network?") 
-@click.option("--net_format", "-nf", default="graphml", type=str, required=False, 
+@click.option("--net_format", "-nf", default="graphml", type=str, 
               help="file format of output network. Can be one of 'edgelist', 'pajek', 'ncol', 'lgl', 'graphml', 'dimacs', 'gml', 'dot', 'leda'")
 # Versioning.
 @click.option('--version', '-v', is_flag=True, 
@@ -81,9 +73,20 @@ def main(support_data_folder, res_folder, test_path, convert2folder, use_existin
         click.echo(f"{bold_package_name} ({__version__})")
         return
 
-    # Click turns the multiple input into tuple. Make it as list now.
-    fisher_geneset = list(fisher_geneset)
-    kde_cutoff = list(kde_cutoff)
+   # Assert that kde_cutoff is a list and contains only floats.
+   assert (
+      isinstance(kde_cutoff, list) and all(isinstance(item, float) for item in kde_cutoff)
+   ), (
+      "kde_cutoff must be a list of floats.")
+
+   # Assert that fisher_geneset is a list and contains only the allowed geneset label.
+   allowed_labels = {"C", "F", "D", "P", "R", "K", "RT", "B"}
+   assert (
+      isinstance(fisher_geneset, list) and all(item in allowed_labels for item in fisher_geneset)
+   ), (
+      "Variable must be a list containing only specific strings: C, F, D, P, R, K, RT, B"
+      )
+
         
     # Create ini_pos and ini_neg, by default or by reading the ini_path file. 
     if(ini_path == ""):
