4e1e08c0c2b3e750a2118f1405560a2e5618b053
hchen_codon <hchen@ebi.ac.uk>
2024-03-14 17:24:32 +0000

    Implement a customized GroupedOptions class; implement click command group; Substaintially rewrote the CLI.py code, making separate cli for each application. Finished and tested phuego_mock

--- phuego/CLI.py
+++ phuego/CLI.py
@@ -2,61 +2,69 @@
 
 from .utils_CLI import load_test_example
 from .utils_CLI import PythonLiteralOption
+from .utils_CLI import GroupedOptions
 from .phuego import phuego
 from .phuego_mock import phuego_mock
 import click
 
-
-__version__ = "1.1.0"
-
-
-'''
-This is the CLI tool for phuego.
-'''
+VERSION = '1.1.0'
 
 # changes the default parameters to -h and --help instead of just --help
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.command(context_settings=CONTEXT_SETTINGS)
-# Folders.
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version=VERSION, prog_name='phuEGO', message='%(prog)s version %(version)s')
+def cli():
+    """phuEGO documentation: https://phuego.readthedocs.io/en/latest/index.html"""
+    pass
+
+def register_command(command_func):
+    """Decorator to register commands with the CLI."""
+    cli.add_command(command_func)
+    return command_func
+
+def grouped_options(**groups):
+    """Decorator to attach grouped options metadata to a command function."""
+    def wrapper(func):
+        func.grouped_options = groups
+        return func
+    return wrapper
+
+
+
+@register_command
+@click.command(cls=GroupedOptions)
+@grouped_options(
+    PATHS=['support_data_folder','res_folder'],
+    SEED_PROPAGATION=['damping_seed_propagation','rwr_threshold'],
+    EGO_DECOMPOSITION_CLUSTERING=['damping_ego_decomposition','kde_cutoff', 'damping_module_detection'],
+    FISHER_TEST=['fisher_geneset', 'fisher_threshold', 'fisher_background'],
+    OUTPUT=['net_format', 'convert2folder']
+)
+# Paths.
 @click.option("--support_data_folder", "-sf", type=str, 
               help="Folder that stored support data. Directories separated by /")
 @click.option("--res_folder", "-rf", type=str, 
               help="Folder that stored output result. Directories separated by /")
-@click.option("--test_path", "-tpath", default="", type=str, 
-              help="The path of user input test file. If defined, -mock and -test should not be used.")
-@click.option("--ini_path", "-ipath", default="", type=str, 
-              help="The path of a csv file, that specify two sets of nodes to be"
-                   " removed from reference network during propagation.")
-@click.option("--convert2folder/--dont_convert2folder", default=True, 
-              help="Should phuego organize result files into a folder structure?")
-# Mock/test.
-@click.option("--run_mock", is_flag=True, 
-              help=("Should phuego perform a mock test run using test data?"
-                    " If defined, -test and -tpath would be ignored."))
-@click.option("--run_test", is_flag=True, 
-              help=("Should phuego perform a full test run using test data?"
-                    " If defined, -tpath would be ignored."))
-# network propagation parameters.
-@click.option("--use_existing_rwr", "-ru", is_flag=True, 
-              help="Should phuego reuse existing network propagation results?")
+# Seed propagation.
 @click.option("--damping_seed_propagation", "-ds", default=0.85, type=float, 
               help=("Damping factor of pagerank algorithm for seeds propagation,"
                     " Float number within range [0.5, 0.95]"))
 @click.option("--rwr_threshold", "-rt", default=0.05, type=float, 
               help=("Threshold of significance for propagated nodes."
                     " Float number within range [0.01, 0.1]"))
-# ego propagation and fisher test.
+# Ego decomposition and clustering.
 @click.option("--damping_ego_decomposition", "-de", default=0.85, type=float,
               help=("Damping factor of pagerank algorithm for ego decomposition,"
                     " Float number within range [0.5, 0.95]"))
-@click.option("--damping_module_detection", "-dm", default=0.85, type=float,
-              help=("Damping factor of pagerank algorithm for module detection,"
-                    " Float number within page [0.5, 0.95]"))
 @click.option("--kde_cutoff", "-k", cls=PythonLiteralOption, default="[0.85]", 
                help=("KDE cutoff value for removing nodes in ego network that are less similar to seed."
                      " Provided as a double quoted list of float number(s). Example: -k \"[0.5, 0.75]\"."
                      " Value should be within range [0.5,0.95]"))
+@click.option("--damping_module_detection", "-dm", default=0.85, type=float,
+              help=("Damping factor of pagerank algorithm for module detection,"
+                    " Float number within page [0.5, 0.95]"))
+# Fisher test.
 @click.option("--fisher_geneset", "-fg", cls=PythonLiteralOption, default="['K']", 
                help=("Genesets to be tested when annotating modules of propagation."
                      " Provided as a quoted list of string(s). Example: -fg \"['C','F']\"."
@@ -66,127 +74,154 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               help="Threshold of significance of geneset enrichment analysis") 
 @click.option("--fisher_background", "-fb", default="intact", type=str,
               help="Geneset background of geneset enrichment analysis") 
-# network output
-@click.option("--include_isolated_egos_in_kde_net", "-ie", is_flag=True, 
-              help="Should we include isolated nodes in the output network?") 
+# Output configuration.
 @click.option("--net_format", "-nf", default="graphml", type=str, 
               help=("file format of output network. Can be one of"
                     " 'edgelist', 'pajek', 'ncol', 'lgl', 'graphml', 'dimacs', 'gml', 'dot', 'leda'"))
-# Versioning.
-@click.option('--version', '-v', is_flag=True, 
-              help='Print version to stdout')
-def main(support_data_folder, res_folder, test_path, convert2folder, use_existing_rwr, run_test, run_mock, 
-         damping_seed_propagation, damping_ego_decomposition, damping_module_detection, 
-         fisher_geneset, fisher_threshold, fisher_background, kde_cutoff, ini_path, rwr_threshold, 
-         include_isolated_egos_in_kde_net, net_format, version) -> None:
+@click.option("--convert2folder/--dont_convert2folder", default=True, 
+              help="Should phuego organize result files into a folder structure?")
+def run_mock(support_data_folder, res_folder, 
+    damping_seed_propagation, rwr_threshold, 
+    damping_ego_decomposition, kde_cutoff, damping_module_detection, 
+    fisher_geneset, fisher_threshold, fisher_background, 
+    net_format, convert2folder):
     """
-    phuEGO documentation: https://phuego.readthedocs.io/en/latest/index.html
+    Perform a mock run with test dataset.
     """
+    test_path, test_df = load_test_example()
+    print("Run phuego_mock with test dataset, whose first few lines are: \n",test_df.head())
 
-    """
-    Print the version number.
-    """  
-    if version:
-        bold_package_name = click.style("phuego", bold=True)
-        click.echo(f"{bold_package_name} ({__version__})")
-        return
+    # Mock run does not allow removing nodes from reference network.
+    ini_pos = ["False"]
+    ini_neg = ["False"]
+    # Mock run does not allow reusing existing options.
+    use_existing_rwr = False
+    # Mock run does not allow excluding isolated egos.
+    include_isolated_egos_in_kde_net = True
 
-
-    """
-    User input formatting and assertion.
-    """
-    # Assert that kde_cutoff is a list and contains only floats.
-    assert (
-       isinstance(kde_cutoff, list) and all(isinstance(item, float) for item in kde_cutoff)
-    ), (
-       "kde_cutoff must be a list of floats.")
- 
-    # Assert that fisher_geneset is a list and contains only the allowed geneset label.
-    allowed_labels = {"C", "F", "D", "P", "R", "K", "RT", "B"}
-    assert (
-       isinstance(fisher_geneset, list) and all(item in allowed_labels for item in fisher_geneset)
-    ), (
-       "Variable must be a list containing only specific strings: C, F, D, P, R, K, RT, B"
-       )
-
-    # Create ini_pos and ini_neg, by default or by reading the ini_path file. 
-    if(ini_path == ""):
-       ini_pos = ["False"]
-       ini_neg = ["False"]
-    else:
-       f1=open(ini_path)
-       ini_pos = f1.readline().strip()
-       ini_pos = ini_pos.split(",")
-       ini_neg = f1.readline().strip()
-       ini_neg = ini_neg.split(",")
+    phuego_mock(
+        support_data_folder=support_data_folder,
+        res_folder=res_folder,
+        test_path=test_path,
+        fisher_geneset=fisher_geneset,
+        fisher_threshold=fisher_threshold,
+        fisher_background=fisher_background,
+        ini_pos=ini_pos,
+        ini_neg=ini_neg,
+        damping_seed_propagation=damping_seed_propagation,
+        damping_ego_decomposition=damping_ego_decomposition,
+        damping_module_detection=damping_module_detection,
+        kde_cutoff=kde_cutoff,
+        use_existing_rwr=use_existing_rwr,
+        convert2folder=convert2folder,
+        include_isolated_egos_in_KDE_net=include_isolated_egos_in_kde_net,
+        net_format=net_format,
+        )
 
 
-    """
-    Run phuego.
-    """
-    if(run_mock):
-           test_path, test_df = load_test_example()
-           print("Run phuego_mock with test dataset, whose first few lines are: \n",test_df.head())
-           phuego_mock(
-              support_data_folder=support_data_folder,
-              res_folder=res_folder,
-              test_path=test_path,
-              fisher_geneset=fisher_geneset,
-              fisher_threshold=fisher_threshold,
-              fisher_background=fisher_background,
-              ini_pos=ini_pos,
-              ini_neg=ini_neg,
-              damping_seed_propagation=damping_seed_propagation,
-              damping_ego_decomposition=damping_ego_decomposition,
-              damping_module_detection=damping_module_detection,
-              kde_cutoff=kde_cutoff,
-              use_existing_rwr=use_existing_rwr,
-              convert2folder=convert2folder,
-              include_isolated_egos_in_KDE_net=include_isolated_egos_in_kde_net,
-              net_format=net_format,
-              )
-    elif(run_test):
-           test_path, test_df = load_test_example()
-           print("Run phuego with test dataset, whose first few lines are: \n",test_df.head())
-           phuego(
-              support_data_folder=support_data_folder,
-              res_folder=res_folder,
-              test_path=test_path,
-              fisher_geneset=fisher_geneset,
-              fisher_threshold=fisher_threshold,
-              fisher_background=fisher_background,
-              ini_pos=ini_pos,
-              ini_neg=ini_neg,
-              damping_seed_propagation=damping_seed_propagation,
-              damping_ego_decomposition=damping_ego_decomposition,
-              damping_module_detection=damping_module_detection,
-              rwr_threshold=rwr_threshold,
-              kde_cutoff=kde_cutoff,
-              use_existing_rwr=use_existing_rwr,
-              convert2folder=convert2folder,
-              include_isolated_egos_in_KDE_net=include_isolated_egos_in_kde_net,
-              net_format=net_format,
-              )
-    elif(test_path):
-           print("Run phuego with user input dataset")
-           phuego(
-              support_data_folder=support_data_folder,
-              res_folder=res_folder,
-              test_path=test_path,
-              fisher_geneset=fisher_geneset,
-              fisher_threshold=fisher_threshold,
-              fisher_background=fisher_background,
-              ini_pos=ini_pos,
-              ini_neg=ini_neg,
-              damping_seed_propagation=damping_seed_propagation,
-              damping_ego_decomposition=damping_ego_decomposition,
-              damping_module_detection=damping_module_detection,
-              rwr_threshold=rwr_threshold,
-              kde_cutoff=kde_cutoff,
-              use_existing_rwr=use_existing_rwr,
-              convert2folder=convert2folder,
-              include_isolated_egos_in_KDE_net=include_isolated_egos_in_kde_net,
-              net_format=net_format,
-              )
-    else:
-           print("Nothing is provided for running phuego.")
+# @register_command
+# @click.command(cls=GroupedOptions)
+# @grouped_options(
+#     Paths=[],
+#     Seed_propagation=[],
+#     Ego_decomposition=[],
+#     Module_detection=[],
+#     Output=[]
+# )
+# def run_test():
+#     """
+#     Perform a test run with test dataset.
+#     """
+#     test_path, test_df = load_test_example()
+#     print("Run phuego with test dataset, whose first few lines are: \n",test_df.head())
+#     phuego(
+#         support_data_folder=support_data_folder,
+#         res_folder=res_folder,
+#         test_path=test_path,
+#         fisher_geneset=fisher_geneset,
+#         fisher_threshold=fisher_threshold,
+#         fisher_background=fisher_background,
+#         ini_pos=ini_pos,
+#         ini_neg=ini_neg,
+#         damping_seed_propagation=damping_seed_propagation,
+#         damping_ego_decomposition=damping_ego_decomposition,
+#         damping_module_detection=damping_module_detection,
+#         rwr_threshold=rwr_threshold,
+#         kde_cutoff=kde_cutoff,
+#         use_existing_rwr=use_existing_rwr,
+#         convert2folder=convert2folder,
+#         include_isolated_egos_in_KDE_net=include_isolated_egos_in_kde_net,
+#         net_format=net_format,
+#         )
+
+# @register_command
+# @click.command(cls=GroupedOptions)
+# @grouped_options(
+#     Paths=[],
+#     Seed_propagation=[],
+#     Ego_decomposition=[],
+#     Module_detection=[],
+#     Output=[]
+# )
+# def run_phos():
+#     """
+#     Run phuEGO by dividing input into three layers: tyrosine kinase, ser/thr kinase, other proteins. 
+#     Recommended for phosphoproteomics dataset.
+#     """
+#     print("Run phuego with user input dataset")
+#     phuego(
+#         support_data_folder=support_data_folder,
+#         res_folder=res_folder,
+#         test_path=test_path,
+#         fisher_geneset=fisher_geneset,
+#         fisher_threshold=fisher_threshold,
+#         fisher_background=fisher_background,
+#         ini_pos=ini_pos,
+#         ini_neg=ini_neg,
+#         damping_seed_propagation=damping_seed_propagation,
+#         damping_ego_decomposition=damping_ego_decomposition,
+#         damping_module_detection=damping_module_detection,
+#         rwr_threshold=rwr_threshold,
+#         kde_cutoff=kde_cutoff,
+#         use_existing_rwr=use_existing_rwr,
+#         convert2folder=convert2folder,
+#         include_isolated_egos_in_KDE_net=include_isolated_egos_in_kde_net,
+#         net_format=net_format,
+#         )
+
+
+# @register_command
+# @click.command(cls=GroupedOptions)
+# @grouped_options(
+#     Paths=[],
+#     Seed_propagation=[],
+#     Ego_decomposition=[],
+#     Module_detection=[],
+#     Output=[]
+# )
+# def run_custom():
+#     """
+#     Run phuEGO by dividing input into user-defined layers (no more than three).
+#     """
+
+
+# @register_command
+# @click.command(cls=GroupedOptions)
+# @grouped_options(
+#     Paths=[],
+#     Seed_propagation=[],
+#     Ego_decomposition=[],
+#     Module_detection=[],
+#     Output=[]
+# )
+# def run_sc():
+#     """
+#     Run phuEGO by dividing input into three layers: receptors, transcription factors, and others.
+#     Recommended for single cell RNA-seq dataset.
+#     """
+
+
+
+
+if __name__ == '__main__':
+    cli()
--- phuego/utils_CLI.py
+++ phuego/utils_CLI.py
@@ -20,3 +20,26 @@ class PythonLiteralOption(click.Option):
             return ast.literal_eval(value)
         except:
             raise click.BadParameter(value)
+
+
+# Group click options for organized help message display.
+class GroupedOptions(click.Command):
+    def format_options(self, ctx, formatter):
+        options = sorted(self.get_params(ctx), key=lambda option: option.opts[0])
+        
+        # Retrieve grouped_options from the callback (the command function)
+        grouped_options = getattr(self.callback, 'grouped_options', {})
+        
+        other_options = [opt for opt in options if opt.name not in sum(grouped_options.values(), [])]
+        
+        for group_name, opt_names in grouped_options.items():
+            opts = [opt for opt in options if opt.name in opt_names]
+            if not opts:
+                continue
+            
+            with formatter.section(group_name):
+                formatter.write_dl([o.get_help_record(ctx) for o in opts])
+
+        if other_options:
+            with formatter.section('Other Options'):
+                formatter.write_dl([o.get_help_record(ctx) for o in other_options])
