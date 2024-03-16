463282b44a441041728897a65e9adb3fded25458
hchen_codon <hchen@ebi.ac.uk>
2024-03-16 11:49:20 +0000

    Rewrite load_seeds and layer_classification to accept any layer classification of up to 3 layers.

--- phuego/load_seeds.py
+++ phuego/load_seeds.py
@@ -2,20 +2,32 @@
 
 import numpy as np
 
-# phuego run-phos; phuego run-mock; phuego run-test.
-def kinase_classification(pfam_domain_path):
-    tyr_kinase=[]
-    st_kinase=[]
-    f1=open(pfam_domain_path)
+def layer_classification(layer_path):
+    layer1=[]
+    layer2=[]
+    f1=open(layer_path)
+
+    # Read first layer definition. 
     seq=f1.readline()
+    # If not defined, return two empty layers.
+    if not seq:
+        return "",[""],"",[""]
     seq=seq.strip().split("\t")
-    tyr_kinase.extend(seq[1:])
+    layer1_name = seq[0]
+    layer1.extend(seq[1:])
+
+    # Read second layer definition. 
     seq=f1.readline()
+    # If not defined, return layer1 and empty layer2.
+    if not seq:
+        return layer1_name, layer1, "", [""]
     seq=seq.strip().split("\t")
-    st_kinase.extend(seq[1:])
-    return tyr_kinase,st_kinase
-    
-def load_seeds(pfam_domain_path, sim_mean_std_path, sim_all_folder_path, 
+    layer2_name = seq[0]
+    layer2.extend(seq[1:])
+
+    return layer1_name, layer1, layer2_name, layer2
+
+def load_seeds(layer_path, sim_mean_std_path, sim_all_folder_path, 
                test_path, graph_nodes):
     '''
     Loading seed nodes.
@@ -44,23 +56,37 @@ def load_seeds(pfam_domain_path, sim_mean_std_path, sim_all_folder_path,
         if seq[1]<-0.0:
             seeds_neg[seq[0]]=-seq[1]
         seq=f1.readline()
-    
+
     '''
-    Classification and semantic similarity.
+    Layer classification and semantic similarity.
     '''
-    # Seed node classification.
-    tyr_kinase,st_kinase=kinase_classification(pfam_domain_path)
-    tyr_pos=list((set(tyr_kinase).intersection(seeds_pos.keys())).intersection(set(graph_nodes)))
-    tyr_neg=list((set(tyr_kinase).intersection(seeds_neg.keys())).intersection(set(graph_nodes)))
-    st_pos= list((set(st_kinase).intersection(seeds_pos.keys())).intersection(set(graph_nodes)))
-    st_neg=list((set(st_kinase).intersection(seeds_neg.keys())).intersection(set(graph_nodes)))
-    all_the_rest_pos=list((set(seeds_pos.keys()).difference(set(tyr_pos+st_pos))).intersection(set(graph_nodes)))
-    all_the_rest_neg=list((set(seeds_neg.keys()).difference(set(tyr_neg+st_neg))).intersection(set(graph_nodes)))
-    seeds=[tyr_pos,st_pos,all_the_rest_pos,tyr_neg,st_neg,all_the_rest_neg]
+    # Classify seed nodes into layers.
+    layer1_name,layer1,layer2_name,layer2=layer_classification(layer_path)
+    if not layer1_name:
+        all_the_rest_pos=list(set(seeds_pos.keys()).intersection(set(graph_nodes)))
+        all_the_rest_neg=list(set(seeds_neg.keys()).intersection(set(graph_nodes)))
+        seeds=[[""],[""],[""],[""],all_the_rest_pos,all_the_rest_neg]
+        print("Input is retained as 1 layer.")
+    elif not layer2_name:
+        layer1_pos=list((set(layer1).intersection(seeds_pos.keys())).intersection(set(graph_nodes)))
+        layer1_neg=list((set(layer1).intersection(seeds_neg.keys())).intersection(set(graph_nodes)))
+        all_the_rest_pos=list((set(seeds_pos.keys()).difference(set(layer1_pos))).intersection(set(graph_nodes)))
+        all_the_rest_neg=list((set(seeds_neg.keys()).difference(set(layer1_neg))).intersection(set(graph_nodes)))
+        seeds=[layer1_pos,layer1_neg,[""],[""],all_the_rest_pos,all_the_rest_neg]
+        print(f"Input is divided into 2 layers: {layer1_name} and all the rest.")
+    else:
+        layer1_pos=list((set(layer1).intersection(seeds_pos.keys())).intersection(set(graph_nodes)))
+        layer1_neg=list((set(layer1).intersection(seeds_neg.keys())).intersection(set(graph_nodes)))
+        layer2_pos=list((set(layer2).intersection(seeds_pos.keys())).intersection(set(graph_nodes)))
+        layer2_neg=list((set(layer2).intersection(seeds_neg.keys())).intersection(set(graph_nodes)))
+        all_the_rest_pos=list((set(seeds_pos.keys()).difference(set(layer1_pos+layer2_pos))).intersection(set(graph_nodes)))
+        all_the_rest_neg=list((set(seeds_neg.keys()).difference(set(layer1_neg+layer2_neg))).intersection(set(graph_nodes)))
+        seeds=[layer1_pos,layer1_neg,layer2_pos,layer2_neg,all_the_rest_pos,all_the_rest_neg]
+        print(f"Input is divided into 3 layers: {layer1_name}, {layer2_name}, and all the rest.")
 
     # Semantic similarity for each seed node.
     ssim={}
-    for i in list(set(tyr_pos+tyr_neg+st_pos+st_neg+all_the_rest_pos+all_the_rest_neg)):
+    for i in list(set(seeds_pos+seeds_neg)):
         ssim[i]={}
         fsim=open(sim_all_folder_path+'/'+i+"_all.txt")
         # The first readline trim away the header.
@@ -70,86 +96,8 @@ def load_seeds(pfam_domain_path, sim_mean_std_path, sim_all_folder_path,
             seq_sim=seq_sim.strip().split("\t")
             ssim[i][seq_sim[1]]=float(seq_sim[2])
             seq_sim=fsim.readline()
-            
+
     '''
     Output.
     '''
     return seeds_pos,seeds_neg,seeds,zscores_global,ssim
-
-
-# phuego run-sc.
-def rc_tf_classification(receptor_tf_path):
-    receptor=[]
-    tf=[]
-    f1=open(receptor_tf_path)
-    seq=f1.readline()
-    seq=seq.strip().split("\t")
-    receptor.extend(seq[1:])
-    seq=f1.readline()
-    seq=seq.strip().split("\t")
-    tf.extend(seq[1:])
-    return receptor,tf
-    
-def load_seeds_sc(receptor_tf_path, sim_mean_std_path, sim_all_folder_path, 
-               test_path, graph_nodes):
-    '''
-    Loading seed nodes.
-    '''
-    # zscores_global.
-    f1 = open(sim_mean_std_path)
-    seq=f1.readline()
-    seq=f1.readline()
-    zscores_global={}
-    while(seq!=""):
-        seq=seq.strip().split("\t")
-        zscores_global[seq[0]]=np.array(seq[1].split("|"),dtype=float)
-        seq=f1.readline()
-
-    # seed nodes.
-    f1 = open(test_path) 
-    seq=f1.readline()
-    ssim={}
-    seeds_pos={}
-    seeds_neg={}
-    while(seq!=""):
-        seq= seq.strip().split("\t")
-        seq[1]=float(seq[1])
-        if seq[1]>0.0:
-            seeds_pos[seq[0]]=seq[1]
-        if seq[1]<-0.0:
-            seeds_neg[seq[0]]=-seq[1]
-        seq=f1.readline()
-    
-    '''
-    Classification and semantic similarity.
-    '''
-    # Seed node classification.
-    receptor,tf=rc_tf_classification(receptor_tf_path)
-    receptor_pos=list((set(receptor).intersection(seeds_pos.keys())).intersection(set(graph_nodes)))
-    receptor_neg=list((set(receptor).intersection(seeds_neg.keys())).intersection(set(graph_nodes)))
-    tf_pos= list((set(tf).intersection(seeds_pos.keys())).intersection(set(graph_nodes)))
-    tf_neg=list((set(tf).intersection(seeds_neg.keys())).intersection(set(graph_nodes)))
-    all_the_rest_pos=list((set(seeds_pos.keys()).difference(set(receptor_pos+tf_pos))).intersection(set(graph_nodes)))
-    all_the_rest_neg=list((set(seeds_neg.keys()).difference(set(receptor_neg+tf_neg))).intersection(set(graph_nodes)))
-    seeds=[receptor_pos,tf_pos,all_the_rest_pos,receptor_neg,tf_neg,all_the_rest_neg]
-
-    # Semantic similarity for each seed node.
-    ssim={}
-    for i in list(set(receptor_pos+receptor_neg+tf_pos+tf_neg+all_the_rest_pos+all_the_rest_neg)):
-        ssim[i]={}
-        fsim=open(sim_all_folder_path+'/'+i+"_all.txt")
-        # The first readline trim away the header.
-        seq_sim=fsim.readline()
-        seq_sim=fsim.readline()
-        while(seq_sim!=""):
-            seq_sim=seq_sim.strip().split("\t")
-            ssim[i][seq_sim[1]]=float(seq_sim[2])
-            seq_sim=fsim.readline()
-            
-    '''
-    Output.
-    '''
-    return seeds_pos,seeds_neg,seeds,zscores_global,ssim
-
-
-# phuego run-custom.
